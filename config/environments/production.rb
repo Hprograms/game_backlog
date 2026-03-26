52f45516e52c045491f33229e7b691cd6a7d92b7
Hikari <hika122025@gmail.com>
2026-03-26 10:49:36 +0900

    Mailgun設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,15 +57,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://game-backlog.onrender.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: ENV["APP_HOST"] }
   ActionMailer::Base.smtp_settings = {
-    :port           => 2525,
-    :address        => 'smtp.mailgun.org',
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+    port:           587,
+    address:        'smtp.mailgun.org',
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         ENV['APP_HOST'],
+    authentication: :plain,
   }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
